4a606ea40cdb3e0fbaff4898c2cbb4e9638d55d9
Jim Clark <slimslenderslacks@gmail.com>
2018-05-23 17:49:50 -0700

    update to 0.5.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.atomist/automation-client-clj "0.5.1"
+(defproject com.atomist/automation-client-clj "0.5.3"
   :description "Atomist automation client implementation in Clojure"
   :url "https://github.com/atomisthq/automation-client-clj"
   :license {:name "Eclipse Public License"
