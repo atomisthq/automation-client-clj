aed463fc58dbaa54b4c8400be1ad2ae67f781f4e
James Carnegie <james@atomist.com>
2018-06-06 22:55:20 +0100

    Echo version

--- atomist.sh
+++ atomist.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export VERSION=$(head -n 1 project.clj | cut -d ' ' -f3 | cut -d '"' -f2 | cut -d '-' -f1)-$(date -u '+%Y%m%d%H%M%S')
 
+echo "Building lein project with version $VERSION"
 lein do test, jar
