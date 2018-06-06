ec15746df0626e16c53e8cf4f5a03cf2e6117a4f
James Carnegie <james@atomist.com>
2018-06-06 23:13:05 +0100

    More death

--- atomist.sh
+++ atomist.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
+die(){
+  echo $1
+  exit 1
+}
+
 export VERSION=$(head -n 1 project.clj | cut -d ' ' -f3 | cut -d '"' -f2 | cut -d '-' -f1)-$(date -u '+%Y%m%d%H%M%S')
 
 echo "Building lein project with version $VERSION"
-lein do test, jar
-lein deploy clojars
+lein do test, jar || die "Build failed"
+lein deploy clojars || die "Deploy failed"
